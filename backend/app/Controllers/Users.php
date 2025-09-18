326fe35c2af84b5346549134b7b70b63e8e77082
Tabipo <danielebarcelon3@gmail.com>
2025-09-18 17:10:13 +0800

    feat(landing page): updated the landing page
    - added a new 'controller'
    - updated routes to route to the llanding page

---
+++ backend/app/Controllers/Users.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+    public function index(): string
+    {
+        return view('user/landing');
+        //can ADD HERE more functions for different views or routes/endpoints
+    }
+}

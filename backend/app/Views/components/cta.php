95c5781be64004c25432941ebcfb97f77d6e6db1
Tabipo <danielebarcelon3@gmail.com>
2025-10-23 02:33:58 +0800

    feat(fragmentation): added new component file
    - added cta file
    - created design and layout of cta

---
+++ backend/app/Views/components/cta.php
@@ -0,0 +1,12 @@
+<div class="col-4">
+    <div class="shadow-sm h-100 card" id="info-card"">
+         <div class="text-center card-body">
+        <h5 style="font-weight: 600;" class="card-title">Midnighter Cafe App</h5>
+        <p class="card-text">Keep up with the latest deals!</p>
+        <?= view('components/buttons/button-primary.php', [
+            'id' => 'primary-btn',
+            'value' => 'Download Now!'
+        ]); ?>
+    </div>
+</div>
+</div>

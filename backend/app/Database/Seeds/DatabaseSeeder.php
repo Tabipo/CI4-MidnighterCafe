5451386a7f4f91f9f71660ef01e3672f8fd388b4
Tabipo <danielebarcelon3@gmail.com>
2025-10-23 15:29:49 +0800

    feat( users table seeder): added a seeder for users table
    - created UsersSeeder file
    - updated DatabaseSeeder file
    - updated CLearDatabaseSeeder file
    - added data to table fields using seeder

--- backend/app/Database/Seeds/DatabaseSeeder.php
+++ backend/app/Database/Seeds/DatabaseSeeder.php
@@ -8,6 +8,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call('App\\Database\\Seeds\\ClearDatabaseSeeder');
+        $this->call('App\\Database\\Seeds\\UsersSeeder');
     }
 }
